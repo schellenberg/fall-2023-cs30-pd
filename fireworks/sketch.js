e73ece52aaaaf22d1457df0bc9d3bb3f9ec1827a
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2023-11-09 19:59:32 -0600

    Update sketch.js

--- fireworks/sketch.js
+++ fireworks/sketch.js
@@ -8,16 +8,21 @@ function setup() {
 }
 
 function draw() {
-  background(220);
-  for (let i = 0; i < theFireworks.length; i++) {
+  background("black");
+  for (let i = theFireworks.length - 1; i >= 0; i--) {
     theFireworks[i].update();
     theFireworks[i].display();
+    if (theFireworks[i].isDead()) {
+      theFireworks.splice(i, 1);
+    }
   }
 }
 
 function mousePressed() {
-  let theFirework = new Firework(mouseX, mouseY);
-  theFireworks.push(theFirework);
+  for (let i = 0; i < 50; i++) {
+    let theFirework = new Firework(mouseX, mouseY);
+    theFireworks.push(theFirework);
+  }
 }
 
 class Firework {
@@ -27,16 +32,26 @@ class Firework {
     this.radius = 5;
     this.dx = random(-5, 5);
     this.dy = random(-5, 5);
-    this.color = "red";
+    this.r = 255;
+    this.g = 0;
+    this.b = 0;
+    this.alpha = 255;
+  }
+
+  isDead() {
+    return this.alpha <= 0;
   }
 
   update() {
     this.x += this.dx;
     this.y += this.dy;
+
+    //make more transparent every frame
+    this.alpha -= 1;
   }
 
   display() {
-    fill(this.color);
+    fill(this.r, this.g, this.b, this.alpha);
     circle(this.x, this.y, this.radius*2);
   }
 }
